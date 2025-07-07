9ed467a12395bf009db8eea1b278729ce18fbab0
mohammad <mgharebaghi92@gmail.com>
2025-07-07 16:50:50 +0330

    change timing

--- src/make_trx/mod.rs
+++ src/make_trx/mod.rs
@@ -25,7 +25,7 @@ pub async fn make() {
             .await
             {
                 Ok(_tx) => {
-                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                    tokio::time::sleep(tokio::time::Duration::from_secs(30)).await;
                 }
                 Err(e) => {
                     println!("Error in transaction 1: {}", e);
@@ -48,7 +48,7 @@ pub async fn make() {
             .await
             {
                 Ok(_tx) => {
-                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                    tokio::time::sleep(tokio::time::Duration::from_secs(36)).await;
                 }
                 Err(e) => {
                     println!("Error in transaction 2: {}", e);
